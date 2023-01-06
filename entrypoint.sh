deb8acfd19d6754f6e928b9c9ed9ebf579c38a36
Maximiliano Baez <maximiliano.baez@peperina.io>
2023-01-06 17:34:32 -0300

    fix entrypoint

--- entrypoint.sh
+++ entrypoint.sh
@@ -7,11 +7,11 @@ if [[ -z "$LAMBDA_LAYER_FOLDER" ]]; then
     echo "Layer Folder"
 fi
 
-print $LAMBDA_LAYER_FOLDER
+echo $LAMBDA_LAYER_FOLDER
 
 LAMBDA_TASK_ROOT="/github/workspace/auth_layer"
 
-print $LAMBDA_TASK_ROOT
+echo $LAMBDA_TASK_ROOT
 
 # Build layer
 pip install -r requirements.txt -t python/lib/python3.9/site-packages/
